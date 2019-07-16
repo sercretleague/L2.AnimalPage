6bf084902c390dd52c6ecdd16abd4e1f9c7ea69c
Ivan Bunin <chernobelenkiy@gmail.com>
2019-07-16 21:59:55 +0300

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+const pug = require('pug');
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', (req, res) => {
+  const compiledFunction = pug.compileFile('./views/index.pug');
+  
+  res.send(compiledFunction({
+    name: 'Porosenok'
+  }));
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
---
+++ views/readme.md
@@ -0,0 +1,6 @@
+1) Нужно установить node.js (https://nodejs.org/en/download/)
+2) Проверить
+- npm --version
+- node --version
+3) npm install
+4) npm start
