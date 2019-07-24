a84bf6dd50fff87a5951b3d93cba04d86a3f2d57
Ivan Bunin <chernobelenkiy@gmail.com>
2019-07-24 19:51:52 +0300

    add form parser

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const bodyParser = require('body-parser');
 
 app.use(express.static(path.join(__dirname, './public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   const compiledFunction = pug.compileFile('./views/index.pug');
@@ -13,5 +15,13 @@ app.get('/', (req, res) => {
     name: 'Porosenok'
   }));
 });
+
+app.post('/submit', (req, res) => {
+  const compiledFunction = pug.compileFile('./views/index.pug');
+
+  res.send(compiledFunction({
+    name: JSON.stringify(req.body)
+  }));
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
